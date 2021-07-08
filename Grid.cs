3fd1c874d3723b0123110b37b506c216009f0022
Hugo Carvalho <huguinho66@live.com>
2021-07-08 18:50:06 +0100

    grid fill added

--- Grid.cs
+++ Grid.cs
@@ -1,12 +1,29 @@
+using System;
+
 namespace LP2_Recurso
 {
     public class Grid
     {
         private Piece[,] pieces;
+        Random rnd;
 
         public Grid(int x, int y)
         {
             pieces = new Piece[x, y];
+            rnd = new Random();
+        }
+
+        public void Fill()
+        {
+            int genPiece;
+
+            for (int i = 0; i < pieces.GetLength(0); i++)
+                for (int j = 0; j < pieces.GetLength(1); j++)
+                {
+                    genPiece = rnd.Next(0, 4);
+
+                    pieces[i, j] = (Piece)genPiece;
+                }
         }
     }
 }
