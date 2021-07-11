f55bd04a6d6df7ab07b5d6c02ef58c60fbb962f7
Hugo Carvalho <huguinho66@live.com>
2021-07-11 18:04:43 +0100

    Unity setup Added

---
+++ UnityApp/Assets/Scripts/UnityApp/Controller.cs
@@ -0,0 +1,22 @@
+﻿using LP2_Recurso;
+using UnityEditor;
+using UnityEngine;
+
+public class Controller : MonoBehaviour, IController
+{
+    public void TogglePause()
+    {
+        
+    }
+    
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#endif
+
+#if UNITY_STANDALONE
+        Application.Quit(0);
+#endif
+    }
+}
